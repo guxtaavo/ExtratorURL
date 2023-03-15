f9b06952de62e689395075988ce67acbe3bb6e99
Gustavo Nunes Lopes <gustaavo.nunesl@gmail.com>
2023-03-15 10:57:12 -0300

    Atualizado

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     nome_parametro = input("Digite o nome do parametro que você queira saber o valor: ")
     extrator_url = ExtratorURL(url, nome_parametro)
     print('O tamanho da url é:', len(extrator_url))
+    print(extrator_url)
 
 if __name__ == "__main__":
     main()
