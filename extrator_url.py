bfcc40c6ad63b396058a5b2b3a98d7874124a22a
Gustavo Nunes Lopes <gustaavo.nunesl@gmail.com>
2023-03-15 10:50:22 -0300

    Programa Funcionando

--- extrator_url.py
+++ extrator_url.py
@@ -1,15 +1,25 @@
+import re
+
 class ExtratorURL():
     def __init__(self, url, nome_parametro):
         self.url = self.santiza_url(url)
         self.valida_url()
-        print(self.get_valor_parametro(nome_parametro))
+        self.get_valor_parametro(nome_parametro)
 
     def santiza_url(self, url):
-        return url.strip()
+        if type(url) == str:
+            return url.strip()
+        else:
+            return ""
 
     def valida_url(self):
         if self.url == "":
             raise ValueError('A URL está vazia.')
+
+        padrao_url = re.compile('(http(s)?://)?(www.)?[a-z]{1,20}.com(.br)?/[a-z]{1,20}')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida.")
     
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -30,3 +40,13 @@ class ExtratorURL():
         else:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
+
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL base: " + self.get_url_base()
+
+    def __eq__(self, other):
+        return self.url == other.url
+        # Utilizado para comparar dois objetos
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from extrator_url import ExtratorURL
 def main():
     url = input("Digite o URL: ")
     nome_parametro = input("Digite o nome do parametro que você queira saber o valor: ")
-    ExtratorURL(url, nome_parametro)
+    extrator_url = ExtratorURL(url, nome_parametro)
+    print('O tamanho da url é:', len(extrator_url))
 
 if __name__ == "__main__":
     main()
